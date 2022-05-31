edc2543f9984410d686189a6591e775ef2617ffe
khumayunkh <hasanovhumayun15@gmail.com>
2022-06-01 03:45:56 +0500

    class

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 console.log('hello'.toUpperCase())
+
+// let a = {
+//     name:'Humoyun'
+// }
+
+// let y = Object.create(a)
+// console.log(y) 
+
+
+class Animals{
+    constructor(name, age, color){
+        this.name = name,
+        this.age = age,
+        this.color = color
+    }
+}
+
+
+let cat = new Animals('cat', 5, 'black')
+console.log(cat.name) //cat
+
+dog = new Animals('dog', 3, 'silver')
+console.log(dog)

99af410adc0b5c8eebee2a487e825b8c5caa0954
khumayunkh <hasanovhumayun15@gmail.com>
2022-06-01 07:13:18 +0500

    obj

--- index.js
+++ index.js
@@ -115,52 +115,106 @@
 // console.log(sha.student) // My name is Sha. I study at 10 form
 // console.log(sha.sayHello)
 
+// class Students{
+//     constructor(student){
+//         // this._name = student._name,
+//         this.form = student.form
+//         this.age = student.age
+//     }
+//     #url = 'https//khumayunkh.com'
+//     url(){
+//         return this.#url
+//     }
+//     set name(name){
+//         this._name = name.trim().toLowerCase()
+//     }
+//     get name(){
+//         return this._name +' '+ this.age
+//     }
+//     get student(){
+//         return this._name + ' ' + this.age + ' ' + this.form
+//     }
+// }
+
+// let humoyun = new Students({
+//     // _name:'Humoyun',
+//     age: 19,
+//     form: 11
+// })
+
+// humoyun.name = '       HUMOYUN'
+// console.log(humoyun.name) // humoyun 19
+// console.log(humoyun.url()) 
+// console.log(humoyun.student)
+// console.log(humoyun._name) // humoyun
+
+// function o(){
+//     return this.x + this.y
+// }
+
+// let obj ={
+//     x:4,
+//     y:8
+// }
+
+// let g = o.bind(obj)
+// console.log(g()) // 12
+
+// const x = (x,y) => x+y
+
+// let p = x.bind(null,3)
+// console.log(p(2)) // 5
+
+
 class Students{
     constructor(student){
-        // this._name = student._name,
+        // this.name = student.name
         this.form = student.form
-        this.age = student.age
-    }
-    #url = 'https//khumayunkh.com'
-    url(){
-        return this.#url
     }
     set name(name){
-        this._name = name.trim().toLowerCase()
+        this._name = name
     }
-    get name(){
-        return this._name +' '+ this.age
-    }
-    get student(){
-        return this._name + ' ' + this.age + ' ' + this.form
+    get name_of_student(){
+        return `my name is ${this._name}`
     }
 }
+
 
 let humoyun = new Students({
-    // _name:'Humoyun',
-    age: 19,
-    form: 11
+    form:12
 })
+humoyun.name = 'Humoyun'
+console.log(humoyun)
+console.log(humoyun.name_of_student.toUpperCase()) // MY NAME IS HUMOYUN
 
-humoyun.name = '       HUMOYUN'
-console.log(humoyun.name) // humoyun 19
-console.log(humoyun.url()) 
-console.log(humoyun.student)
-console.log(humoyun._name) // humoyun
 
-function o(){
-    return this.x + this.y
+let sha  = new Students(humoyun)
+sha.name = 'sha'
+console.log(sha)
+console.log(sha.name_of_student)
+
+let array = [1,2,3,2,3,4,5,6]
+let d = array.filter(item => item >4)
+console.log(d)
+
+let x = Array.from(array)
+x[0]=5
+console.log(x)
+
+let obj = {
+    name: 'humoyun',
+    age:19
 }
 
-let obj ={
-    x:4,
-    y:8
+if(obj.name ==='humoyun'){
+    for(let k in obj)
+    console.log(obj[k]+2)
 }
 
-let g = o.bind(obj)
-console.log(g()) // 12
+let h = 5
+let y =1
+for (let i = 1; i<=h; i++){
+    y *= i 
+}
 
-const x = (x,y) => x+y
-
-let p = x.bind(null,3)
-console.log(p(2)) // 5
+console.log(y) //120
